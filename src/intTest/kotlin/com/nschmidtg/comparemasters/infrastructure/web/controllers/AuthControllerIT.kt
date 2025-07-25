c2300fde3c78bb26620467d25c76caeaa383f9a8
Nicolas Schmidt <nschmidtg@gmail.com>
2025-07-25 21:45:28 +0200

    assertTrue instead of assert

--- src/intTest/kotlin/com/nschmidtg/comparemasters/infrastructure/web/controllers/AuthControllerIT.kt
+++ src/intTest/kotlin/com/nschmidtg/comparemasters/infrastructure/web/controllers/AuthControllerIT.kt
@@ -9,6 +9,7 @@ import com.nschmidtg.comparemasters.infrastructure.web.viewmodels.Authentication
 import com.nschmidtg.comparemasters.infrastructure.web.viewmodels.AuthenticationResponse
 import com.nschmidtg.comparemasters.infrastructure.web.viewmodels.RegistrationRequest
 import java.time.Instant
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -147,7 +148,7 @@ class AuthControllerIT {
                 AuthenticationResponse::class.java
             )
 
-        assert(response.token == oldToken)
+        assertEquals(oldToken, response.token)
     }
 
     @Test
